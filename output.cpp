a77e8d95d8ccaa8a83b338f16ee9505bad44665c
Pavel Statrovoitov <pollstar27@gmail.com>
2022-10-27 23:00:01 +0300

    Refactoring files to output class

--- output.cpp
+++ output.cpp
@@ -5,13 +5,14 @@
  *      Author: Pavel Starovoitov
  */
 
-#include <output.h>
+#include "output.h"
 
 namespace gpio
 {
 
   output::output (gpio::port *const port, const uint32_t pin, const uint32_t speed,
-	  const uint32_t outputType, const uint32_t pull) : pin::pin(port, pin)
+	  const uint32_t outputType, const uint32_t pull)
+    : pin::pin(port, pin)
   {
     LL_GPIO_InitTypeDef GPIO_InitStruct = {
 	Pin: _pin,
@@ -24,6 +25,40 @@ namespace gpio
     init(&GPIO_InitStruct);
 
   }
+
+void output::set (void) const
+  {
+#ifdef USE_FULL_LL_DRIVER
+    LL_GPIO_SetOutputPin (_port->getPort (), _pin);
+#elif USE_HAL_DRIVER
+    pin::set();
+#else
+  #error Need to implement the init method
+#endif
+  }
+
+void output::reset (void) const
+  {
+#ifdef USE_FULL_LL_DRIVER
+    LL_GPIO_ResetOutputPin (_port->getPort (), _pin);
+#elif USE_HAL_DRIVER
+    pin::reset();
+#else
+  #error Need to implement the init method
+#endif
+  }
+
+gpio::state output::get (void)
+{
+#ifdef USE_FULL_LL_DRIVER
+  return  LL_GPIO_IsOutputPinSet(_port->getPort (), _pin) ? gpio::state::HIGH : gpio::state::LOW;
+#elif USE_HAL_DRIVER
+  GPIO_PinState state = HAL_GPIO_ReadPin (_port->getPort (), _pin);
+  return state == GPIO_PIN_SET ? gpio::state.HIGH : gpio::state.LOW;
+#else
+#error Need to implement the init method
+#endif
+}
 
   output::~output ()
   {
--- output.h
+++ output.h
@@ -8,7 +8,7 @@
 #ifndef OUTPUT_H_
 #define OUTPUT_H_
 
-#include <pin.h>
+#include "pin.h"
 
 namespace gpio
 {
@@ -20,7 +20,9 @@ namespace gpio
   public:
     output (gpio::port *const port, const uint32_t pin, const uint32_t speed = gpio::speed_low,
 	    const uint32_t outputType = LL_GPIO_OUTPUT_PUSHPULL, const uint32_t pull = LL_GPIO_PULL_NO);
-    virtual
+    virtual void set (void) const;
+    virtual void reset (void) const;
+    virtual gpio::state get (void);
     ~output ();
   };
 
